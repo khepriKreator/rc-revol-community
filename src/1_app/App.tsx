1b85f69b01fa5e7a0e9c12a4f4aa926536be6f03
Fedor Makareev <fm@alnair.ae>
2024-03-07 23:59:34 -0300

    fix: prettier config

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,17 +1,22 @@
-import {
-  QueryClientProvider,
-    QueryClient
-} from '@tanstack/react-query'
-import {RouterProvider} from 'react-router-dom'
-import {routerConfig} from '../2_pages'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import { routerConfig } from '../2_pages';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 function App() {
   return (
-      <QueryClientProvider client={queryClient}>
-            <RouterProvider router={routerConfig}/>
-      </QueryClientProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routerConfig} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- src/2_pages/routerConfig.tsx
+++ src/2_pages/routerConfig.tsx
@@ -1,37 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom';
 import {
-    createBrowserRouter,
-} from "react-router-dom";
-import {PlayerStat, TracksList, TrackStat, HomePage, FaqPage} from "./index.ts";
-import {Layout} from "../3_widgets/";
+  PlayerStat,
+  TracksList,
+  TrackStat,
+  HomePage,
+  FaqPage,
+} from './index.ts';
+import { Layout } from '../3_widgets/';
 
 export const routerConfig = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                element: <HomePage />,
-                index: true
-            },
-            {
-                path: 'tracks',
-                element: <TracksList/>,
-            },
-            {
-                element: <PlayerStat/>,
-                path: 'account/:accountId'
-            },
-            {
-                element: <TrackStat/>,
-                path: 'tracks/:trackId',
-            },
-            {
-                element: <FaqPage/>,
-                path: '/faq',
-            }
-        ]
-    }
-])
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        element: <HomePage />,
+        index: true,
+      },
+      {
+        path: 'tracks',
+        element: <TracksList />,
+      },
+      {
+        element: <PlayerStat />,
+        path: 'account/:accountId',
+      },
+      {
+        element: <TrackStat />,
+        path: 'tracks/:trackId',
+      },
+      {
+        element: <FaqPage />,
+        path: '/faq',
+      },
+    ],
+  },
+]);
 /**
  * 1. /home -
  * 2. /faq
@@ -39,4 +43,3 @@ export const routerConfig = createBrowserRouter([
  * 4. /tracks/:trackId
  * 5. /account/:accountId
  * */
-
